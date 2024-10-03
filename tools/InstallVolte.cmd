2e12b25b8b02d9c2620c940c16f8949c728e450d
Iceows <mounierr07@gmail.com>
2024-10-03 14:13:01 +0200

    tools: change dir permissions for HuaweiIMS

--- tools/InstallVolte.cmd
+++ tools/InstallVolte.cmd
@@ -5,7 +5,8 @@ java.exe -jar "ApkSigner.jar" sign  --key platform.pk8 --cert platform.x509.pem 
 adb root
 adb remount rw, /system
 adb shell mkdir /system/app/HuaweiIMS
-adb push HuaweiIMS.apk /system/app/HuaweiIMS
+adb shell chmod 755 /system/app/HuaweiIMS
+adb push HuaweiIMS.apk /system/app/HuaweiIMS/
 adb shell chmod 644 /system/app/HuaweiIMS/HuaweiIMS.apk 
 
 
